437f952eae042a840e56c9db1a39d7f77207ddd0
Rayhan <nofussdufus@gmail.com>
2021-12-15 08:31:14 +0000

    Add API calls for adding and getting scores

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import './style.css';
-import scores from './scores.js';
 
 const scoresTable = document.querySelector('#tableOfScores');
 
-const displayScores = () => {
+const endpoint = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games"
+const gameId = "MmPje8nXd7xg0GvcGIEk"
+
+async function getScores(){
+  let data =  await fetch(`${endpoint}/${gameId}/scores`)
+  let response = await data.json()
+  let results = await response.result
+  return results
+}
+
+function displayScore(score) {
+  const tableRow = document.createElement('tr');
+  tableRow.innerHTML = `<td> ${score.user}: ${score.score}</td>`;
+  scoresTable.appendChild(tableRow);
+}
+
+async function displayAllScores() {
+  let scores = await getScores()
   scores.forEach((score) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `<td> ${score.name}: ${score.score}</td>`;
-    scoresTable.appendChild(tableRow);
+    displayScore(score)  
   });
 };
 
-displayScores();
+async function addScore(user,score){
+  let data = await fetch(`${endpoint}/${gameId}/scores`,
+  {
+    method:'POST',
+    body: JSON.stringify({
+        'user' : user,
+        'score' : score
+    }),
+    mode: 'cors',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+  let response = await data.json()
+  let result = await response.result
+  if (result.includes("Leaderboard score created correctly")) displayScore({score:score,user:user})
+}
+
+displayAllScores();
