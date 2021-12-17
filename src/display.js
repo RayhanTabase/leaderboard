0bf96430a048d1b2aac71a744b9d161213204533
Rayhan <nofussdufus@gmail.com>
2021-12-17 15:58:00 +0000

    Change functions to arrow functions

--- src/display.js
+++ src/display.js
@@ -3,24 +3,24 @@ import { getScores } from './apiScores.js';
 const scoresTable = document.querySelector('#tableOfScores');
 
 // Display loading icon
-function loadingContent(show) {
+const loadingContent = (show) => {
   const loadingContent = document.querySelector('#loadingContent');
   if (show) {
     loadingContent.style.display = 'flex';
     return;
   }
   loadingContent.style.display = 'none';
-}
+};
 
 // Display a score on the page
-function displayScore(score) {
+const displayScore = (score) => {
   const tableRow = document.createElement('tr');
   tableRow.innerHTML = `<td> ${score.user}: ${score.score}</td>`;
   scoresTable.appendChild(tableRow);
-}
+};
 
 // Display all the scores on the page
-async function displayAllScores() {
+const displayAllScores = async () => {
   loadingContent(true);
   scoresTable.innerHTML = '';
   const scores = await getScores();
@@ -28,17 +28,17 @@ async function displayAllScores() {
     displayScore(score);
   });
   loadingContent(false);
-}
+};
 
 // Display error message on incorrect form
-function displayError(display) {
+const displayError = (display) => {
   const errorMessage = document.querySelector('.errorMessage');
   if (display) errorMessage.style.display = 'block';
   else errorMessage.style.display = 'none';
-}
+};
 
 // Display notifications
-function displayNotification(message) {
+const displayNotification = (message) => {
   const notificationElement = document.querySelector('#notificationMessage');
   const notification = document.createElement('p');
   notification.classList.add('fadeMessage');
@@ -48,7 +48,7 @@ function displayNotification(message) {
   notification.addEventListener('animationend', () => {
     notification.remove();
   });
-}
+};
 
 export {
   displayScore, displayAllScores, displayError, displayNotification,
